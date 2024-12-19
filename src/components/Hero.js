764364ec76acf11260d891cc417ae91cb68575bb
gkganesh95 <gk95520@gmail.com>
2024-12-19 10:23:19 +0530

    added git link

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,13 @@
 import HeroImg from '../assets/hero.png'
-import { AiOutlineInstagram,AiOutlineFacebook,AiOutlineTwitter, AiOutlineLinkedin } from "react-icons/ai";
+import { AiOutlineGithub, AiOutlineInstagram,AiOutlineLinkedin } from "react-icons/ai";
 export default function Hero() {
     const config = {
         subtitle: 'Im a Front-End Developer',
         social: {
             instagram: 'https://www.instagram.com/gk_ganesh95/?igsh=MWxkNXZ3cHpydmNkcA%3D%3D#',
-            linkedin: 'https://www.linkedin.com/authwall?trk=bf&trkInfo=AQF87IcC-qmLUQAAAZPbR5XQb860LUpQ3g40G4XEwhC25CFT1lzzP77muSqcMimXRRSwfyW7h3IeQYTFUwXVc19pXak3AYy3OShI6al6A02m5dulmNRJFOSr5-UINhnAjT-PA_E=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fganesh-kumar-4a5628279%3Futm_source%3Dshare%26utm_campaign%3Dshare_via%26utm_content%3Dprofile%26utm_medium%3Dandroid_ap'
+            linkedin: 'https://www.linkedin.com/authwall?trk=bf&trkInfo=AQF87IcC-qmLUQAAAZPbR5XQb860LUpQ3g40G4XEwhC25CFT1lzzP77muSqcMimXRRSwfyW7h3IeQYTFUwXVc19pXak3AYy3OShI6al6A02m5dulmNRJF1OSr5-UINhnAjT-PA_E=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fganesh-kumar-4a5628279%3Futm_source%3Dshare%26utm_campaign%3Dshare_via%26utm_content%3Dprofile%26utm_medium%3Dandroid_ap',
+            github: 'https://github.com/gkganesh95'
+
         }
     }
     return <section className='flex flex-col md:flex-row px-5 py-32 bg-primary justify-center'>
@@ -15,6 +17,7 @@ export default function Hero() {
         <div className='flex py-10'>
             <a href={config.social.instagram} className='pr-5 hover:text-white'><AiOutlineInstagram size={40}/></a>
             <a href={config.social.linkedin}className='hover:text-white'><AiOutlineLinkedin size={40}/></a>
+            <a href={config.social.github}className='hover:text-white'><AiOutlineGithub size={40}/></a>
         </div>
         </div>
         <img className='md:w-1/3' src={HeroImg}/>
